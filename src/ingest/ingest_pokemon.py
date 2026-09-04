11eb8ba3ba457165597dce1fb8d4bc81044b2c13
jam <jamesdreyes19@gmail.com>
2026-09-04 01:01:10 -0700

    create get_pokemon_varieites and loop_all_pokemon method

--- src/ingest/ingest_pokemon.py
+++ src/ingest/ingest_pokemon.py
@@ -21,5 +21,35 @@ def get_pokemon_data(url):
     else:
         print(f"Failed to retrieve data. Status code: {response.status_code}")
 
+def get_pokemon_varieties(pokemon_name):
+    # create url 
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{pokemon_name.lower()}"
+    
+    response = requests.get(url)
+    
+    # request successful
+    if response.status_code == 200:
+        data = response.json()
+        
+        print(f"Name: {data['name'].capitalize()}")
+        print(f"ID: {data['id']}")
+        print(f"Varieties: {[variety['pokemon']['name'] for variety in data['varieties']]}")
+        for variety in data['varieties']:
+            variety_url = variety['pokemon']['url']
+            if should_include(variety['pokemon']['name']):
+                get_pokemon_data(variety_url)
+            else:
+                print(f"Excluded variety: {variety['pokemon']['name']}")
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+
+def loop_all_pokemon():
+    url = "https://pokeapi.co/api/v2/pokemon?limit=2000"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        for pokemon in data['results']:
+            get_pokemon_varieties(pokemon['name'])
+
 if __name__ == "__main__":
-    get_pokemon_data("https://pokeapi.co/api/v2/pokemon/1")
+    get_pokemon_varieties("pikachu") 
