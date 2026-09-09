2b2f2d07fe87632c834925014bba4badb71a5a55
jam <jamesdreyes19@gmail.com>
2026-09-08 21:20:09 -0700

    create get_variety_names function

--- src/ingest/ingest_pokemon.py
+++ src/ingest/ingest_pokemon.py
@@ -77,6 +77,14 @@ def parse_pokemon(data: dict) -> dict:
         "sprite": data['sprites']['front_default']
     }
 
+def get_variety_names(species_name: str) -> list[str]:
+    url = f"https://pokeapi.co/api/v2/pokemon-species/{species_name.lower()}"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    return [variety["pokemon"]["name"] for variety in data["varieties"]]
+
 if __name__ == "__main__":
     test = fetch_pokemon("venusaur")
     print(parse_pokemon(test))
+    print(get_variety_names("pikachu"))
